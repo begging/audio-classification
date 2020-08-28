57ec21fdc15c41dd4760ce18abdcd3c156e91390
Rudy <rudy.dev.kor@gmail.com>
2020-08-28 14:42:53 +0900

    Update get_proper_rate func
     - Return supported rate list

--- utils/check_mic.py
+++ utils/check_mic.py
@@ -1,9 +1,11 @@
 import pyaudio as pa
+import numpy as np
 
 p = pa.PyAudio()
 
 
-rate_list = [8000, 11025, 16000, 22050, 32000, 44100, 48000, 88200, 96000]
+rate_list = [8000, 11025, 16000, 22050, 32000, 37800, 44100, 47250, 48000,
+             50000, 50400, 64000, 88200, 96000]
 
 def print_device_info():
     info = p.get_host_api_info_by_index(0)
@@ -33,6 +35,16 @@ def get_supported_rate(device_idx):
                 available_rate_list.append(rate)
 
     return available_rate_list
+
+
+def get_proper_rate(supported_rate_list, target_rate):
+
+    supported_rate_arr = np.array(supported_rate_list)
+    preprocessed = np.abs(np.subtract(supported_rate_arr, target_rate))
+    min_idxs = np.where(preprocessed == np.amin(preprocessed))
+    min_idx = min_idxs[0][-1]
+
+    return supported_rate_list[min_idx]
 
 
 if __name__ == '__main__':
